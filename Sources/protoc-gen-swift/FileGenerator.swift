3ecf50bd5f62198377e2a3655fb5acca612f7b30
Rishav Gupta <rishav.gupta@gojek.com>
2023-05-23 11:26:46 +0530

    commenting this as it uses it implicitely - https://www.avanderlee.com/swift/sendable-protocol-closures/

--- Sources/protoc-gen-swift/FileGenerator.swift
+++ Sources/protoc-gen-swift/FileGenerator.swift
@@ -150,16 +150,17 @@ class FileGenerator {
             m.generateMainStruct(printer: &p, parent: nil, errorString: &errorString)
         }
 
-        var sendablePrinter = CodePrinter(p)
-        for m in messages {
-            m.generateSendable(printer: &sendablePrinter)
-        }
+        // commenting this as it uses it implicitely - https://www.avanderlee.com/swift/sendable-protocol-closures/
+        // var sendablePrinter = CodePrinter(p)
+        // for m in messages {
+        //     m.generateSendable(printer: &sendablePrinter)
+        // }
 
-        if !sendablePrinter.isEmpty {
-            p.print("", "#if swift(>=5.5) && canImport(_Concurrency)")
-            p.append(sendablePrinter)
-            p.print("#endif  // swift(>=5.5) && canImport(_Concurrency)")
-        }
+        // if !sendablePrinter.isEmpty {
+        //     p.print("", "#if swift(>=5.5) && canImport(_Concurrency)")
+        //     p.append(sendablePrinter)
+        //     p.print("#endif  // swift(>=5.5) && canImport(_Concurrency)")
+        // }
 
         if !extensionSet.isEmpty {
             let pathParts = splitPath(pathname: fileDescriptor.name)
